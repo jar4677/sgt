24a94de0bf97ec1411f094383bee4a9b6a781555
root <root@ip-172-31-10-227.us-west-2.compute.internal>
2016-06-09 18:37:54 +0000

    merged

--- apis/add_student.php
+++ apis/add_student.php
@@ -16,10 +16,9 @@ if(empty($_POST)){
     $name = sanitize($_POST['name']);
     $assignment = sanitize($_POST['assignment']);
     $grade = sanitize($_POST['grade']);
-<<<<<<< HEAD
 
     $query = "INSERT INTO `data` (`name`, `assignment`, `grade`) VALUES ('$name', '$assignment', '$grade')";
-    
+
     if(mysqli_query($conn, $query)){
 	print($query);
 	print('successful');
@@ -27,13 +26,3 @@ if(empty($_POST)){
 	print('unsuccessful');
     };
 };
-=======
-
-    $query = "INSERT INTO `data` (`name`, `assignment`, `grade`) VALUES ('$name', '$assignment', '$grade')";
-
-    print(mysqli_query($conn, $query));
-};
-
-
-
->>>>>>> 7f96746eeb0b3a29e55002837dade54ebdc0cf2d
